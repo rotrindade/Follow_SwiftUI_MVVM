0bc98d925a0af162995d0b301f330cdbc487e08a
Rodrigo Trindade <rodrigotrindade@protonmail.com>
2023-06-20 10:50:23 -0300

    Initial Commit

--- Follow_SwiftUI_MVVM/ContentView.swift
+++ Follow_SwiftUI_MVVM/ContentView.swift
@@ -1,26 +1,69 @@
+//  ContentView.swift
+//  Follow_MVVM
+//
+//  Created by Rodrigo Trindade on 6/15/23.
+//
+
 //
 //  ContentView.swift
-//  Follow_SwiftUI_MVVM
+//  Follow_MVVM
 //
-//  Created by Rodrigo Trindade on 20/06/23.
+//  Created by Rodrigo Trindade on 6/15/23.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct ProfileView: View {
     var body: some View {
+        
+        //Perfil
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("profile_stevejobs")
+                .resizable()
+                .frame(width: 250, height: 250)
+                .padding(.bottom, 10)
+            
+            Text("Steve Jobs")
+                .font(.system(size: 50, weight: .bold))
+            
+            Text("@stevejobs")
+                .font(.system(size: 23, weight: .regular))
+                .foregroundStyle(.gray)
+            
+            Text("22.643K")
+                .font(.system(size: 70, weight: .light))
+                .padding(40)
+            
+            //Buttons
+            VStack{
+                
+                //Seguir
+                Button{} label: {
+                    Label("Follow", systemImage: "person.crop.circle.badge.plus")
+                        .font(.title3)
+                        .frame(maxWidth: .infinity)
+                }
+                .buttonStyle(.borderedProminent)
+                .controlSize(.large)
+                
+                //Enviar Mensagem
+                Button{} label: {
+                    Label("enviar mensagem", systemImage: "paperplane.fill")
+                        .font(.title3)
+                        .frame(maxWidth: .infinity)
+                }
+                .buttonStyle(.borderedProminent)
+                .controlSize(.large)
+                .disabled(true)
+            }
+            .padding(20)
         }
-        .padding()
+        
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ProfileView()
     }
 }
--- Follow_SwiftUI_MVVM/Follow_SwiftUI_MVVMApp.swift
+++ Follow_SwiftUI_MVVM/Follow_SwiftUI_MVVMApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Follow_SwiftUI_MVVMApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ProfileView()
         }
     }
 }
