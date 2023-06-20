a1a2bed2c388d3a583d24849e35bc155cb6471a2
Rodrigo Trindade <rodrigotrindade@protonmail.com>
2023-06-20 10:45:27 -0300

    Initial Commit

---
+++ Follow_SwiftUI_MVVM/Follow_SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Follow_SwiftUI_MVVMApp.swift
+//  Follow_SwiftUI_MVVM
+//
+//  Created by Rodrigo Trindade on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Follow_SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
